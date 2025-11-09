2c1427a28a5cec4f80915eb63d868717a438cd92
AsmaaEbrhimes <zerowepschool@gmail.com>
2025-11-10 01:35:50 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,27 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Header {
   constructor() {}
+  toggelHeader = signal<boolean>(false);
+  // visible: boolean = false;
+  // visible2: boolean = false;
+  // visibleDrawer: boolean = false;
 
-  visible: boolean = false;
-  visible2: boolean = false;
-  visibleDrawer: boolean = false;
+  // showDialog() {
+  //   this.visible = true;
+  //   this.visibleDrawer = false;
+  // }
 
-  showDialog() {
-    this.visible = true;
-    this.visibleDrawer = false;
-  }
+  // showDialogLogin() {
+  //   this.visible2 = true;
+  //   this.visibleDrawer = false;
+  // }
 
-  showDialogLogin() {
-    this.visible2 = true;
-    this.visibleDrawer = false;
-  }
+  // CloseDilog() {
+  //   this.visible = false;
+  //   this.visible2 = false;
+  // }
 
-  CloseDilog() {
-    this.visible = false;
-    this.visible2 = false;
-  }
+  // ShowDraw() {
+  //   this.visibleDrawer = true;
+  // }
 
-  ShowDraw() {
-    this.visibleDrawer = true;
+  onToggelMenue() {
+    this.toggelHeader.set(!this.toggelHeader());
   }
 }
